b7908a8cdd35a3ad6f782c7de8a0a3f006af7359
Arazz <91001726+Arazzq@users.noreply.github.com>
2022-03-17 20:28:52 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+class Config:
 
+    API_ID = "6775827"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+
+    API_HASH = "05607067317e01a39ed1e5e1d21dce12" # 🤖 Get It From My.telegram.org.
+
+    BOT_TOKEN = "5235594088:AAGzUmwgCAQ6qjGdImUrWV5tXLZw4Lgjw_Q" # 🤖 Get it from @botfather with /newbot command
